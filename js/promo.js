63662a9f19d59ec1d584bd151675587cc5f057c6
bmccenter <77107786+bmccenter@users.noreply.github.com>
2021-01-11 15:05:39 +0700

    Update promo.js

--- js/promo.js
+++ js/promo.js
@@ -1,3 +1,7 @@
 <script>
-  document.write("PROMO Hari Sabtu & Minggu, Masa Pakai Voucher kusus 2rb=3jam, 3rb=4jam, 5rb=10jam. Promo cuma ada di 3 voucher tersebut.")
-</script>
+        // membuat objek elemen
+        var hasil = document.getElementById("promo");
+
+        // menampilkan output ke elemen hasil
+        hasil.innerHTML = "PROMO Hari Sabtu & Minggu, Masa Pakai Voucher kusus 2rb=3jam, 3rb=4jam, 5rb=10jam. Promo cuma ada di 3 voucher tersebut.";
+    </script>
